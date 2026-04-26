3c00130f29f4aa75834adfa1efdcba504fb17ada
vishnu5789 <mvishnuvardhan5789@gmail.com>
2026-04-26 19:46:40 +0530

    These are the files I have chnagesd in episode 2

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-const parant = React.createElement("div",
-     {id:"parant"}, 
-     React.createElement("div", {id:"child"},
-          [[React.createElement("h1",{id:"child-h1"},"child-1"), React.createElement('h2',{},'Child-2')],
-     [React.createElement("h1",{id:"child-h1"},"child-1"), React.createElement('h2',{},'Child-2')]]))
-// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React Element")
-console.log(parant)
-const root = ReactDOM.createRoot(document.getElementById("root"))
-console.log(root)
-root.render(parant)
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+
+  React.createElement(
+    "div",
+    { id: "child" },
+
+    [
+      React.createElement(
+        "div",
+        { key: "row1" },
+        [
+          React.createElement("h1", { key: "row1-h1" }, "Child-1"),
+          React.createElement("h2", { key: "row1-h2" }, "Child-2")
+        ]
+      ),
+
+      React.createElement(
+        "div",
+        { key: "row2" },
+        [
+          React.createElement("h1", { key: "row2-h1" }, "Child-3"),
+          React.createElement("h2", { key: "row2-h2" }, "Child-4")
+        ]
+      )
+    ]
+  )
+);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
